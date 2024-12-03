7a36b43ec927b4d5652d09a7a35603ab926ffddc
kartamasi <kar@physi.uni-heidelberg.de>
2024-12-03 14:38:16 +0100

    path fix

--- examples/Cosmic_GNN/remove_weird_events.py
+++ examples/Cosmic_GNN/remove_weird_events.py
@@ -6,18 +6,18 @@ import os
 import pandas as pd
 
 #Find problematic events
-input_dir = '/mnt/data1/tkar/cosmic_new/fully_connected_graphs/cosmic_michel_f3/test/'
+input_dir = '/mnt/data1/tkar/cosmic_new/fully_connected_graphs/cosmic_michel_f3/aa/'
 
 i = 0
+weird_dir= input_dir+'weird_events/'
 for dataset in ['trainset', 'valset', 'testset']:
-    weird_dir = input_dir+'weird_events/'+dataset+'/'
-    os.makedirs(weird_dir, exist_ok=True)
+    weird_sub_dir = weird_dir+dataset+'/'
+    os.makedirs(weird_sub_dir, exist_ok=True)
 
     for graph_dir in glob.glob(input_dir+dataset+'/*.pyg'):
         graph = torch.load(graph_dir)
 
         if graph.x.shape[0] == graph.track_edges.shape[1]:
             i += 1
-            shutil.move(graph_dir, input_dir+'weird_events/')
-
+            shutil.move(graph_dir, weird_sub_dir)
 print(i)
